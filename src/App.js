1764a55dadeb3b9594640088646806c26f5ae19e
shubhamdobhal05 <shubhamdobhal05@gmail.com>
2022-05-31 21:39:38 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredResults, setFilteredResults] = useState([]);
+
+
+  useEffect(() => {
+    axios.get(`www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
+      .then((response) => {
+        setData(response.data);
+      })
+  }, [])
+
+
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue)
+    if (searchInput !== '') {
+        const filteredData = data.filter((item) => {
+            return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
+        })
+        setFilteredResults(filteredData)
+    }
+    else{
+        setFilteredResults(data)
+    }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" placeholder='Search....' onChange={(e) => searchItems(e.target.value)} />
+
+
+      <div>
+        {filteredResults.map((item) => {
+          return (
+            <div key={item.idMeal}>
+              <img src={item.strMealThumb} alt={item.strMeal}/>
+              <h2>{item.strMeal}</h2>
+              <p>{item.strCategory}</p>
+              <p>{item.strInstructions}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
